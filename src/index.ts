a31a5fded0c2b301191c34a5bd07b8f238f58e17
mvdc1 <48755913+phantom-2@users.noreply.github.com>
2020-05-04 11:47:52 +1000

    linting

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 // tslint:disable-next-line
 const electron = require("electron"); // Enabling Electron in project.
+import * as process from "process";
 const {app, BrowserWindow, Menu, dialog} = electron; // Setting up Electron.
 
-app.on('window-all-closed', () => {{ // Ensuring that the application closes when the close button is selected.
-    app.quit();
-}
+app.on("window-all-closed", () => {
+	{
+		// Ensuring that the application closes when the close button is selected.
+		app.quit();
+	}
 });
 
 app.on("ready", () => {
-
 	let mainWindow = new BrowserWindow({width: 620, height: 200, resizable: false}); // Sets the application window.
 	mainWindow.loadURL("file://" + __dirname + "/pages/index.html"); // Points to file to display.
 
@@ -17,12 +19,11 @@ app.on("ready", () => {
 	});
 
 	Menu.setApplicationMenu(Menu.buildFromTemplate(mainMenuTemplate)); // Setting up main menu.
-
 });
 
 const mainMenuTemplate = [
 	{
-		label: "Clicker Game (" + process.env.user || process.env.username + ")",
+		label: "Clicker Game (" + process.env.username + ")",
 		submenu: [
 			{
 				label: "Leaderboard (beta)",
@@ -32,17 +33,17 @@ const mainMenuTemplate = [
 						buttons: ["Okay"],
 						defaultId: 1,
 						Title: "Information",
-						message: "Apologies, this area is for the app developer only as it is not yet stable."
+						message: "Apologies, this area is for the app developer only as it is not yet stable.",
 					});
-				}
+				},
 			},
 			{
 				label: "Quit Game",
 				accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
 				click() {
 					app.quit();
-				}
-			}
-		]
-	}
-]
+				},
+			},
+		],
+	},
+];
